ffc36b2b011ae8282563de29c391dcb1b5cbc9ec
Nicolas Abraham <niko.hr@proton.me>
2026-07-27 13:33:03 -0400

    CH2 - L1

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+
+    test_node = TextNode("Lorem Ipsum", TextType.PLAIN_TEXT, "www.github.com")
+    print(test_node)
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -14,4 +14,12 @@ class TextNode:
         self.url = url
 
     def __eq__(self, value: object, /) -> bool:
-        pass
+        if not isinstance(value, TextNode):
+            return False
+
+        return(self.text == value.text
+            and self.text_type == value.text_type
+            and self.url == value.url)
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
