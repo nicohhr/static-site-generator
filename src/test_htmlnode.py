6fc31e0588a215d2d3e82cd94ff43ab9ed2c552c
Nicolas Abraham <niko.hr@proton.me>
2026-07-28 21:53:41 -0400

    CH2 - L2 - Test Updates

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,22 +1,39 @@
 import unittest
+
 from htmlnode import HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
 
 
-    def test_repr_n1(self):
+    def test_repr(self):
         n1 = HTMLNode(tag="href", value="gmail.com", props={"node": "test_1"})
-        print(repr(n1))
-
-
-    def test_repr_n2(self):
-        n2 = HTMLNode(tag="h1", value="Main Title", props={"n2": "test_2"})
-        print(repr(n2))
-
+        self.assertEqual(repr(n1), "Tag: href\n" +
+            "Value: gmail.com\n" +
+            "Children: None\n" +
+            "Props: {'node': 'test_1'}\n")
 
     def test_repr_n3(self):
         n1 = HTMLNode(tag="href", value="gmail.com", props={"node": "test_1"})
         n2 = HTMLNode(tag="h1", value="Main Title", props={"n2": "test_2"})
         n3 = HTMLNode(tag="p", value="Something", children=[n1, n2], props={"n3": "test_2"})
-        print(repr(n3))
+        self.assertEqual(repr(n3), "Tag: p\n" +
+            "Value: Something\n" +
+            "Children: [Tag: href\n" +
+            "Value: gmail.com\n" +
+            "Children: None\n" +
+            "Props: {'node': 'test_1'}\n" +
+            ", Tag: h1\n" +
+            "Value: Main Title\n" +
+            "Children: None\n" +
+            "Props: {'n2': 'test_2'}\n" +
+            "]\n" +
+            "Props: {'n3': 'test_2'}\n")
+
+    def test_instance_values(self):
+        n1 = HTMLNode(tag="href", value="gmail.com", props={"node": "test_1"})
+
+        self.assertEqual(n1.tag, "href")
+        self.assertEqual(n1.value, "gmail.com")
+        self.assertEqual(n1.children, None)
+        self.assertEqual(n1.props, {"node": "test_1"})
