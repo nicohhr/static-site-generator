2d4a3bb9f88673d9c1090e41cbdc9716bc7d45d2
Nicolas Abraham <niko.hr@proton.me>
2026-08-04 23:20:59 -0400

    CH2 - Add delimiter splitting scaffold

--- src/markdown_utils.py
+++ src/markdown_utils.py
@@ -1,5 +1,28 @@
 from textnode import TextNode, TextType
 
-def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+def split_test(old_nodes: list[TextNode], delimiter: str):
+    new_nodes: list[TextNode] = []
+    for node in old_nodes:
+        splited_text = node.text.split(delimiter)
+        for slice in splited_text:
+            new_nodes.append(TextNode(slice, TextType.PLAIN))
+    return new_nodes
 
-    pass
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode] | None:
+    return None
+
+def testing_split():
+    res: list[TextNode] = []
+    test_test = ["Hello World, im trying to split 'code' using a delimiter",
+                "Hello World, im trying to split 'code using a delimiter'",
+                "'Hello World', im trying to split code using a delimiter"]
+    for test in test_test:
+        res.append(TextNode(test, TextType.PLAIN))
+
+    for tested_node in split_test(res, "'"):
+        print(tested_node.text + "|")
+
+
+if __name__ == "__main__":
+    testing_split()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import override
 
-from src.leafnode import LeafNode
+from leafnode import LeafNode
 
 
 class TextType(Enum):
